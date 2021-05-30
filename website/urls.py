b4e2d4fbfcf112d1d6d6b140e648da53f8bf0f56
andre13 <abreu.andre13@gmail.com>
2021-05-30 20:11:48 +0100

    chagen urls

--- website/urls.py
+++ website/urls.py
@@ -7,7 +7,7 @@ from django.urls import path
 app_name = "website"
 
 urlpatterns = [
-    path('home', views.home_page_view, name='home'),
+    path('', views.home_page_view, name='home'),
     path('sobre', views.sobre_page_view, name='sobre'),
     path('contatos', views.contatos_page_view, name='contatos'),
 ]
