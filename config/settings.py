353728ac6a79d8beee90cc8c67456cbdcfe3537b
andre13 <abreu.andre13@gmail.com>
2021-05-30 23:18:28 +0100

    chagen urls

--- config/settings.py
+++ config/settings.py
@@ -46,8 +46,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+MIDDLEWARE_CLASSES = (
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+)
 
 ROOT_URLCONF = 'config.urls'
 
